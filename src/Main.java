4ad1ff4c6bbdca440bd6d8dcac356afdf11cfc4f
Aleksey_T <talekseys@mail.ru>
2023-05-06 13:04:50 +0300

    Sdelal

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
+        int start = 0;
+
+        int plus = 1100;
+        int bonus = 0;
+
+        int balans;
+        if (plus > 1000) {
+            bonus = plus / 100;
+            balans = start + plus + bonus;
+        } else {
+            balans = start + plus;
+        }
+
+        System.out.println(" Итоговый баланс: " + balans);
+        System.out.println(" Бонусные рубли: " + bonus);
 
 
-
-        
-
-
-
-               }
     }
+}
